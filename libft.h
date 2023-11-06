3c7be2fea24052bebe6cf2fbc8e9a733ad9924e5
gr3yknight <hamza1talhaoui@gmail.com>
2023-11-06 21:07:28 +0100

    Updated the header file

--- libft.h
+++ libft.h
@@ -38,6 +38,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
+char	**ft_split(char const *s, char c);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
@@ -52,7 +53,6 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-char	**ft_split(char const *s, char c);
 
 //To Do
 
