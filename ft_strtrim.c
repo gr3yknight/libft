58cecae8e37992793a7f523deda3d6a06dedc63c
gr3yknight <hamza1talhaoui@gmail.com>
2023-11-09 02:20:05 +0100

    still needs more edits

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -46,8 +46,8 @@ static int	isTrimmable(char *str, char a)
 {
 	while (*str)
 	{
-		if (a == *str)
-			return 1;
+		if (*str == a)
+			return (1);
 		str++;
 	}
 	return (0);
@@ -55,7 +55,7 @@ static int	isTrimmable(char *str, char a)
 static int	the_counter(char const *s, char const *set)
 {
 	int	counter;
-	int i;
+	int	i;
 	int	j;
 
 	i = 0;
@@ -74,16 +74,20 @@ static int	the_counter(char const *s, char const *set)
 	return (counter);
 }
 
-static int	isEdge(char const *str, char *set)
+static int	isEdge(char const *str, char c)
 {
-	int i = 1;
+	int	x;
+
+	x = 0;
 	while (*str)
 	{
-		if (*str == *set)
+		if (isTrimmable == 1)
+		{
+			x++;
 			str++;
-		set++;
+		}
+		return (x);
 	}
-	return (i);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
@@ -91,9 +95,19 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char			*str;
 	int				len;
 	unsigned int	x;
+	int				bln;
+	int				i;
 
+	x = 0;
+	bln = isTrimmable(s1, set[i]);
+	while (!s1)
+	{
+		if (isTrimmable == 1)
+			x++;
+		return (x);
+	}
 	len = ft_strlen(s1) - the_counter(s1, set);
-	ft_substr(s1, x, len);
+	str = ft_substr(s1, x, len);
 	str[len] = '\0';
 	return (str);
 }
