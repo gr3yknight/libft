bec16c47fb953f8da980e83d53d75999cbf1da6f
gr3yknight <hamza1talhaoui@gmail.com>
2023-11-14 03:18:30 +0100

    New edits, itoa is almost done if not don already

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,29 +12,12 @@
 
 #include "libft.h"
 
-static char	*is_edge(int n)
-{
-	char	*str;
-	int		max;
-	int 	min;
-
-	max = 2147483647;
-	min = -2147483648;
-	if (n == max)
-		str = x;
-	if (n == min)
-		str = x;
-	if (n == 0)
-		str = 0 + 48;
-	return (str);
-}
-
 static int	ft_count(int n)
 {
 	int	ft_counter;
 
 	ft_counter = 0;
-	if (n < 0)
+		if (n < 0)
 		ft_counter += 1;
 	while (n != 0)
 	{
@@ -49,17 +32,14 @@ static char	*is_negative(char *str, int n, int ft_counter)
 	int	i;
 
 	i = ft_counter - 1;
+	n *= -1;
 	while (i > 0)
 	{
-		if (n < 0)
-		{
-			str[0] = '-';
-			n *= -1;
-		}
-		str[i] = n % 10 + 48;
+		str[i] = n % 10 + '0';
 		n /= 10;
 		i--;
 	}
+	str[i] = '-';
 	return (str);
 }
 
@@ -77,27 +57,51 @@ static char	*is_positive(char *str, int n, int ft_counter)
 	return (str);
 }
 
+static char	*is_edge(char *str, int n, int ft_counter)
+{
+	int		max;
+	int 	min;
+	int		i;
+
+	i = 0;
+	max = 2147483647;
+	min = -2147483648;
+	if (n == max)
+	{
+		n = 147483647;
+		str = is_positive(str, n, ft_counter);
+		str[i] = '2';
+	}
+	else if (n == min)
+		{
+			i = 2;
+			n *= -1;
+			n = 147483648;
+			str += i;
+			str = is_positive(str, n, ft_counter); 
+			str[--i] = '2';
+			str[--i] = '-';
+		}
+	return (str);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
 	int		ft_counter;
-
+	
 	ft_counter = ft_count(n);
 	str = malloc (sizeof(char) * (ft_counter + 1));
 	if (str == NULL)
 		return (NULL);
 	str[ft_counter] = '\0';
+	if (n == 0)
+		*str = 0 + '0';
 	if (n < 0)
 		str = is_negative(str, n, ft_counter);
 	if (n > 0)
 		str = is_positive(str, n, ft_counter);
-	if (n == 0)
-		str = is_edge(n);
+	if (n == 2147483647 || n == -2147483648)
+		str = is_edge(str, n, ft_counter);
 	return (str);
-}
-
-int main()
-{
-	printf("[%s]\n", ft_itoa(0));
-	return (0);
 }
