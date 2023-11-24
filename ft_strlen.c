7704ea7b9f86ab20f2df159e5de6cf61cdffcadc
gr3yknight <hamza1talhaoui@gmail.com>
2023-11-24 12:13:18 +0100

    protecting strlen, solving many problems

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hatalhao <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: talhaoui <talhaoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 17:57:15 by hatalhao          #+#    #+#             */
-/*   Updated: 2023/11/03 17:58:51 by hatalhao         ###   ########.fr       */
+/*   Updated: 2023/11/24 12:11:01 by talhaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *str)
 {
 	size_t	i;
 
+	if (!str)
+		return (0);
 	i = 0;
 	while (str[i])
 		i++;
